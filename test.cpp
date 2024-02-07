57a419f4c2c66cb768ebb7bc1bb90bbf4a0a9cd3
Chinmay <chinmayjindal108@gmail.com>
2024-02-07 02:59:05 -0500

    fast i/o

--- test.cpp
+++ test.cpp
@@ -1,9 +1,12 @@
-#include <bits/stdc++.h>
+//#include <bits/stdc++.h>
 
 #include <iostream>
 using namespace std;
 
 int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
     int x;
 
     cin >> x;
