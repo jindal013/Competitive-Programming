2b218504691def65759ba2064f83b92ca7913f68
Chinmay <chinmayjindal108@gmail.com>
2024-02-21 02:25:59 -0500

    add graph algo

---
+++ csesproblems.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int n;
+
+    cin >> n;
+
+    if (n==1){
+        cout << 1 << endl;
+    }
+    else if ((n==2) || (n==3)){
+        cout << "NO SOLUTION" << endl;
+    }
+    else {
+        for (int i = 1; i <= (n/2); i++){
+            cout << (i+(n/2)) << " " << i << " ";
+        }
+        if (n%2 != 0){
+            cout << n;
+        }
+    }
+    
+}
