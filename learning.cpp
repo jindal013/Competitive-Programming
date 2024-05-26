3c53ad7d3a0cb9a0813880a07a46cb0be9993f74
Chinmay <chinmayjindal108@gmail.com>
2024-05-25 20:14:05 -0400

    file io

--- learning.cpp
+++ learning.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
+//#include <Strin
 #include <cstdio>
+#include <vector>
+#include <stack>
+#include <queue>
+
 
 using namespace std;
 
 int main(){
-    freopen("template.in", "r", stdin);
-    freopen("template.out", "w", stdout);
+    //freopen("template.in", "r", stdin);
+    //freopen("template.out", "w", stdout);
 
-    
+    deque<int> d;
+    d.push_front(1);
+    d.push_back(2);
+
+    for (auto it : d){
+        cout << it << typeid(it).name() << endl;
+
+    }
+
+
+
 }
