f307488aac7bc6eba66c22be7281aecb227a3814
Chinmay <chinmayjindal108@gmail.com>
2025-03-05 09:24:08 -0500

    fixed tle error

--- CSES/graph/flightroutes.cpp
+++ CSES/graph/flightroutes.cpp
@@ -5,15 +5,29 @@ using namespace std;
 
 int n, m;
 vector<vector<int>> adj;
+vector<vector<int>> adj2;
 vector<bool> visited;
 
-void dfs(int s){
-    if (visited[s]) return;
+int dfs(int s){
+    if (visited[s]) return 0;
     visited[s] = true;
+    int num = 1;
     
     for (int u : adj[s]){
-        dfs(u);
+        num += dfs(u);
     }
+    return num;
+}
+
+int dfs2(int s){
+    if (visited[s]) return 0;
+    visited[s] = true;
+    int num = 1;
+    
+    for (int u : adj2[s]){
+        num += dfs2(u);
+    }
+    return num;
 }
 
 int main(){
@@ -23,25 +37,57 @@ int main(){
     
     cin >> n >> m;
     adj.assign(n, {});
+    adj2.assign(n, {});
     visited.assign(n, false);
 
     for (int i = 0; i < m; i++){
         cin >> a >> b;
-        adj.at(--a).push_back(--b);
+        adj[--a].push_back(--b);
+        adj2[b].push_back(a);
     }
 
-    for (int i = 0; i < n; i++){
-        visited.assign(n, false);
-        // cout << "RUNNING THIS LOOP" << endl;
-        dfs(i);
+    if (dfs(0) != n){
+        cout << "NO" << endl;
         for (int j = 0; j < n; j++){
             if (!visited[j]){
-                cout << "NO" << endl;
-                cout << i + 1 << " " << j + 1 << endl;
+                cout << 1 << " " << j + 1 << endl;
                 return 0;
             }
         }
     }
+    else{
+        visited.assign(n, false);
+        if (dfs2(0) != n){
+            cout << "NO" << endl;
+            for (int j = 0; j < n; j++){
+                if (!visited[j]){
+                    cout << j + 1 << " " << 1 << endl;
+                    return 0;
+                }
+            }
+        }
+    }
+
+    // for (int i = 0; i < n; i++){
+    //     visited.assign(n, false);
+    //     // cout << "RUNNING THIS LOOP" << endl;
+    //     if (dfs(i) != n){
+    //         cout << "NO" << endl;
+    //         for (int j = 0; j < n; j++){
+    //             if (!visited[j]){
+    //                 cout << i + 1 << " " << j + 1 << endl;
+    //                 return 0;
+    //             }
+    //         }
+    //     }
+    //     // for (int j = 0; j < n; j++){
+    //     //     if (!visited[j]){
+    //     //         cout << "NO" << endl;
+    //     //         cout << i + 1 << " " << j + 1 << endl;
+    //     //         return 0;
+    //     //     }
+    //     // }
+    // }
 
     cout << "YES" << endl;
 }
